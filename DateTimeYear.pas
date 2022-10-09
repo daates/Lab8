9df9e4e842765330a16b6efe5cf31a56391821c1
имя фамилия <114353007+daates@users.noreply.github.com>
2022-10-09 11:26:33 +0300

    Добавлено gitignore и два файла паскаль

---
+++ DateTimeYear.pas
@@ -0,0 +1,13 @@
+﻿{
+Проверка года на високосность
+}
+begin
+  var year := readinteger('Введите год:');
+  Assert(year > 0);
+  if (year mod 4 = 0) and (year mod 100 <> 0) then
+    println(year, 'год - високосный!')
+  else if (year mod 4 = 0) and (year mod 100 = 0) and (year mod 400 = 0) then
+    println(year, 'год - високосный!')
+  else
+    println(year, 'год - не високосный!')
+end.
