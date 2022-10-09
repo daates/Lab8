91bb8b40c7826dc5a234937b69104fc6d7902e18
имя фамилия <114353007+daates@users.noreply.github.com>
2022-10-09 11:54:10 +0300

    Add DateTimeSumDay

---
+++ DateTimeSumDay.pas
@@ -0,0 +1,27 @@
+﻿{
+сколько дней с годах
+}
+function year(a:integer): boolean;
+var y: boolean;
+begin
+  if (a mod 4 = 0) and (a mod 100 <> 0) then
+    y := True
+  else if (a mod 4 = 0) and (a mod 100 = 0) and (a mod 400 = 0) then
+    y := True
+  else
+    y := False;
+end;
+
+begin 
+  var sum := 0;
+  var year1 := readinteger('Введите 1 год:');
+  var year2 := readinteger('Введите 2 год:');
+  for var i := year1 to year2 do
+    begin
+      if year(i) then
+        sum += 366
+      else
+        sum += 365;
+    end;
+  println($'C {year1} год по {year2} год сумарно {sum} дней!');
+end.
